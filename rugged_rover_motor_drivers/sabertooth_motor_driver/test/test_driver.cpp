3730c5a5bfd4dfa1c14bcb7031395b7e49a9950e
Reece Holland <reece.j.holland@gmail.com>
2025-07-16 08:36:17 +0800

    driver unit tests

---
+++ rugged_rover_motor_drivers/sabertooth_motor_driver/test/test_driver.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include "sabertooth_motor_driver/driver.hpp"
+
+using sabertooth_motor_driver::Driver;
+
+
+TEST(DriverTests, ComputeChecksumWorks)
+{
+  std::vector<uint8_t> data = {0xFF, 0x06, 0x01, 0x00, 0x00};
+  uint8_t checksum = Driver::computeChecksum(data);
+  EXPECT_EQ(checksum, 0xF8);
+}
+
+TEST(DriverTests, PacketPackingLE)
+{
+  std::vector<uint8_t> buffer;
+  Driver::packInt16LE(buffer, 0x1234);
+  ASSERT_EQ(buffer.size(), 2);
+  EXPECT_EQ(buffer[0], 0x34);
+  EXPECT_EQ(buffer[1], 0x12);
+}
