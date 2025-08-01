cbba1f41de2a2756f4ae370df52f9f07b239e458
Reece Holland <reece.j.holland@gmail.com>
2025-08-02 07:15:46 +0800

    modifications for bringup

---
+++ rugged_rover_bringup/launch/bringup.launch.py
@@ -0,0 +1,74 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    # Paths to packages and files
+    description_pkg = FindPackageShare("rugged_rover_robot_description")
+    control_pkg = FindPackageShare("rugged_rover_control")
+
+    # Xacro to URDF
+    xacro_path = PathJoinSubstitution([
+        FindPackageShare('rugged_rover_robot_description'),
+        'urdf',
+        'rugged_rover.urdf.xacro'
+    ])
+
+
+    # Convert xacro output into a raw string using Command + ParameterValue
+    # robot_description = {
+    #     "robot_description": ParameterValue(
+    #         Command(["xacro", xacro_file]),
+    #         value_type=str
+    #     )
+    # }
+
+    controllers_yaml = PathJoinSubstitution([
+        control_pkg,
+        "config",
+        "controllers.yaml"
+    ])
+
+    return LaunchDescription([
+        Node(
+            package="micro_ros_agent",
+            executable="micro_ros_agent",
+            name="micro_ros_agent",
+            arguments=["serial", "--dev", "/dev/ttyACM0"],
+            output="screen"
+        ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[{'robot_description': ParameterValue(
+                    Command(['xacro ', xacro_path]),
+                    value_type=str
+                )}],
+            output="screen"
+        ),
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            name="controller_manager",
+            parameters=[{'robot_description': ParameterValue(
+                    Command(['xacro ', xacro_path]),
+                    value_type=str
+                )}, controllers_yaml],
+            output="screen"
+        ),
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+            output="screen"
+        ),
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["diff_drive_controller", "--controller-manager", "/controller_manager"],
+            output="screen"
+        )
+    ])
--- rugged_rover_control/launch/control.launch.py
+++ rugged_rover_control/launch/control.launch.py
@@ -1,32 +1,67 @@
 from launch import LaunchDescription
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-    config_path = os.path.join(
-        get_package_share_directory('rugged_rover_control'),  # <-- Change this to your actual package
-        'config',
-        'controllers.yaml'
-    )
+    # Paths to packages and files
+    description_pkg = FindPackageShare("rugged_rover_robot_description")
+    control_pkg = FindPackageShare("rugged_rover_control")
+
+    # Xacro to URDF
+    xacro_file = PathJoinSubstitution([
+        description_pkg,
+        "urdf",
+        "rugged_rover.urdf.xacro"
+    ])
+
+    # Convert xacro output into a raw string using Command + ParameterValue
+    robot_description = {
+        "robot_description": ParameterValue(
+            Command(["xacro", xacro_file]),
+            value_type=str
+        )
+    }
+
+    controllers_yaml = PathJoinSubstitution([
+        control_pkg,
+        "config",
+        "controllers.yaml"
+    ])
 
     return LaunchDescription([
         Node(
+            package="micro_ros_agent",
+            executable="micro_ros_agent",
+            name="micro_ros_agent",
+            arguments=["serial", "--dev", "/dev/ttyACM0"],
+            output="screen"
+        ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[robot_description],
+            output="screen"
+        ),
+        Node(
             package="controller_manager",
             executable="ros2_control_node",
-            parameters=[config_path],
+            name="controller_manager",
+            parameters=[robot_description, controllers_yaml],
             output="screen"
         ),
         Node(
             package="controller_manager",
             executable="spawner",
-            arguments=["joint_state_broadcaster"],
+            arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
             output="screen"
         ),
         Node(
             package="controller_manager",
             executable="spawner",
-            arguments=["diff_drive_controller"],
+            arguments=["diff_drive_controller", "--controller-manager", "/controller_manager"],
             output="screen"
         )
     ])
