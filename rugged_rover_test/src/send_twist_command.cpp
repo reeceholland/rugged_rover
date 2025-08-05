f64e7bd8bfb99b756dcc333110aac1a7ee23fc50
Reece Holland <reece.j.holland@gmail.com>
2025-08-05 13:02:43 +0800

    clang format

--- rugged_rover_test/src/send_twist_command.cpp
+++ rugged_rover_test/src/send_twist_command.cpp
@@ -1,8 +1,7 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("twist_command_sender");
 
@@ -10,8 +9,7 @@ int main(int argc, char** argv)
 
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>(
       "micro_ros_arduino_twist_subscriber", rclcpp::QoS(10));
-  while (rclcpp::ok())
-  {
+  while (rclcpp::ok()) {
     geometry_msgs::msg::Twist msg;
 
     // Example values – you can adjust these as needed
