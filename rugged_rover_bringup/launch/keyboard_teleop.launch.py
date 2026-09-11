b47bb07d10124bfd8059f16cf1fbb29a16f1029a
Reece Holland <reece.j.holland@gmail.com>
2026-09-11 09:37:12 +0800

    fix keys

--- rugged_rover_bringup/launch/keyboard_teleop.launch.py
+++ rugged_rover_bringup/launch/keyboard_teleop.launch.py
@@ -29,13 +29,14 @@ def generate_launch_description():
 
         ExecuteProcess(
             cmd=[
-                "ros2",
-                "run",
-                "teleop_twist_keyboard",
-                "teleop_twist_keyboard",
-                "--ros-args",
-                "-r",
-                ["/cmd_vel:=", cmd_vel_topic],
+                "bash",
+                "-lc",
+                [
+                    "exec ros2 run teleop_twist_keyboard teleop_twist_keyboard "
+                    "--ros-args -r /cmd_vel:=",
+                    cmd_vel_topic,
+                    " < /dev/tty",
+                ],
             ],
             output="screen",
             emulate_tty=True,
